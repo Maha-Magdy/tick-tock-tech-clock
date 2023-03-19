caf35f038541be8fa92baebb036d7505ef30d37a
Maha Magdy <mahamagdy3@gmail.com>
2023-03-20 01:52:47 +0200

    Add the functionality of the minute hand in the clock

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".minute-hand");
 
 const setDate = () => {
   const now = new Date();
+
   const seconds = now.getSeconds();
   const secondsDegree = (seconds / 60) * 360 + 90;
   secondHand.style.transform = `translateY(-50%) rotate(${secondsDegree}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegree = (minutes / 60) * 360 + 90;
+  minuteHand.style.transform = `translateY(-50%) rotate(${minutesDegree}deg)`;
 };
 
 setInterval(setDate, 1000);
