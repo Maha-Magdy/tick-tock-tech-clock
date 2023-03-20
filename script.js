6fd7324678faf3f5fb8969a6a26e630d73162954
maha-magdy <maha.magdy@reviewsolicitors.com>
2023-03-20 17:26:50 +0000

    Add the hour hand functionality, and fix the transition issue of the second hand after it get 60 seconds done

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const secondHand = document.querySelector(".second-hand");
 const minuteHand = document.querySelector(".minute-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 const setDate = () => {
   const now = new Date();
@@ -7,10 +8,16 @@ const setDate = () => {
   const seconds = now.getSeconds();
   const secondsDegree = (seconds / 60) * 360 + 90;
   secondHand.style.transform = `translateY(-50%) rotate(${secondsDegree}deg)`;
+  secondHand.style.transition = secondsDegree > 440 ? 'unset' : 'all 0.05s';
+  secondHand.style.transitionTimingFunction = secondsDegree > 440 ? 'unset' : 'cubic-bezier(0.1, 2.7, 0.58, 1)';
 
   const minutes = now.getMinutes();
   const minutesDegree = (minutes / 60) * 360 + 90;
   minuteHand.style.transform = `translateY(-50%) rotate(${minutesDegree}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegree = (hours / 12) * 360 + 90;
+  hourHand.style.transform = `translateY(-50%) rotate(${hoursDegree}deg)`;
 };
 
 setInterval(setDate, 1000);
